b6fcf4cffd339aaf01adcbd34d3fa52081a16ab8
RohanGorla <rohangorla2010@gmail.com>
2024-12-03 16:15:25 +0530

    Remove unnecessary code from client side and add Chat App header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,15 @@ import "./App.css";
 const socket = io(`${import.meta.env.VITE_SERVER_URL}`);
 
 function App() {
-  return <></>;
+  return (
+    <div className="Chat_App">
+      <div className="Chat_App--Container">
+        <section className="Chat_App--Header">
+          <h1>Chit-Chat</h1>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default App;
