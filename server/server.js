cf147f2c6c5869699aabfed29e9686918fa4522b
RohanGorla <rohangorla2010@gmail.com>
2024-12-03 13:07:47 +0530

    Made socket io connection in server

--- server/server.js
+++ server/server.js
@@ -5,7 +5,16 @@ import { Server } from "socket.io";
 import { MongoClient } from "mongodb";
 
 const app = express();
-const server = createServer(app);
 const PORT = process.env.PORT;
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Socket connection made...");
+});
 
 server.listen(PORT, () => console.log(`Listening at port ${PORT}`));
