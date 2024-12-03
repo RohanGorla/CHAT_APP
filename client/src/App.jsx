44a99f64f311811abf2f9795ab13d14250a21fef
RohanGorla <rohangorla2010@gmail.com>
2024-12-03 16:49:28 +0530

    Add send message form and socket event in client to send messages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
+import { IoMdSend } from "react-icons/io";
 import "./App.css";
 
 const socket = io(`${import.meta.env.VITE_SERVER_URL}`);
@@ -9,8 +10,15 @@ function App() {
   const [message, setMessage] = useState("");
   const [chat, setChat] = useState([]);
 
-  socket.on("allChat", (paylod) => {
-    console.log(paylod);
+  async function sendMessage(e) {
+    e.preventDefault();
+    socket.emit("message_input", { message });
+  }
+
+  useEffect(() => {
+    socket.on("allChat", (paylod) => {
+      setChat(paylod);
+    });
   });
 
   return (
@@ -19,7 +27,33 @@ function App() {
         <section className="Chat_App--Header">
           <h1>Chit-Chat</h1>
         </section>
-        <section className="Chat_App--Chats"></section>
+        <section className="Chat_App--Chats">
+          <div className="Chat_App--Chat_Messages">
+            {chat.map((message, index) => {
+              return (
+                <div key={index} className="Chat_App--Chat_Message">
+                  <p>{message.name}</p>
+                  <p>{message.msg}</p>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+        <section className="Chat_App--Type_Message">
+          <form onSubmit={sendMessage}>
+            <input
+              type="text"
+              className="Chat_App--Message_Input"
+              value={message}
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+            ></input>
+            <button type="submit">
+              <IoMdSend />
+            </button>
+          </form>
+        </section>
       </div>
     </div>
   );
