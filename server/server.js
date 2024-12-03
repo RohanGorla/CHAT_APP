9e2a02a41a5e38294d9fb797f0e151c8ec79e6ef
RohanGorla <rohangorla2010@gmail.com>
2024-12-03 13:23:29 +0530

    Connected to mongo db in server

--- server/server.js
+++ server/server.js
@@ -4,14 +4,23 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import { MongoClient } from "mongodb";
 
-const app = express();
 const PORT = process.env.PORT;
+
+/* CREATING AND CONNECTING TO SOCKET SERVER */
+const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
   },
 });
+
+/* CONNECTING TO MONGODB CLIENT */
+const client = new MongoClient(process.env.DB_URL);
+client
+  .connect()
+  .then(() => console.log("Mongodb connected..."))
+  .catch((err) => console.log(err));
 
 io.on("connection", (socket) => {
   console.log("Socket connection made...");
