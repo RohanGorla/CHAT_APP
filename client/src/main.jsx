7ccdce69473ea000fa2cabce26cb3803c04d5e79
RohanGorla <rohangorla2010@gmail.com>
2024-12-10 13:36:03 +0530

    Add React router to route all the pages from main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  Route,
+  Router,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import Login from "./Components/Login.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<App />}>
+        
+      </Route>
+    </>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
