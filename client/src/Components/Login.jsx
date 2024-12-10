33921653f91f0657e5acfffc89ab8a7deb92b6ad
RohanGorla <rohangorla2010@gmail.com>
2024-12-10 19:30:29 +0530

    Add login component and add the login form with username and passwrod fields

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "../Styles/Login.css";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>Login</div>
-  )
+    <div className="Login_Page">
+      <form className="Login_Page--Form">
+        <div className="Login_Form--Field">
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          ></input>
+        </div>
+        <div className="Login_Form--Field">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          ></input>
+        </div>
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            // localStorage.setItem("chitchat_username", username);
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
