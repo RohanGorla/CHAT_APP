7c61182a013ca9e8ae3ec532c9ea74a460f6603d
RohanGorla <rohangorla2010@gmail.com>
2024-12-12 22:56:56 +0530

    Add an accept friend requests method in client side

--- client/src/Components/Notifications.jsx
+++ client/src/Components/Notifications.jsx
@@ -7,6 +7,11 @@ function Notifications() {
   const { socket, notifications, setNotifications } = useOutletContext();
   const userData = JSON.parse(localStorage.getItem("ChatApp_UserInfo"));
 
+  /* ACCEPT FRIEND REQUEST SOCKET METHOD */
+  async function acceptRequest(request) {
+    socket.emit("accept_request", request);
+  }
+
   return (
     <div className="Notifications_Page">
       {notifications.map((notification, index) => {
@@ -14,7 +19,9 @@ function Notifications() {
           <div key={index}>
             <p>{notification.from}</p>
             <div>
-              <button>Accept</button>
+              <button onClick={() => acceptRequest(notification)}>
+                Accept
+              </button>
               <button>Decline</button>
             </div>
           </div>
