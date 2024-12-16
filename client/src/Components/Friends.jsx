8111fd8ff4b3a38693632e4df213b6cab03ccc8b
RohanGorla <rohangorla2010@gmail.com>
2024-12-17 05:00:24 +0530

    Add basic display of friends/groups of the user in friends page

--- client/src/Components/Friends.jsx
+++ client/src/Components/Friends.jsx
@@ -17,25 +17,34 @@ function Friends() {
 
   return (
     <div className="Friends_Page">
-      {rooms.map((room, index) => {
-        let roomName;
-        switch (room.type) {
-          case "single":
-            const friendId = room.users.filter(
-              (user) => user !== userData.userId
-            );
-            for (let i = 0; i < friends.length; i++) {
-              if (friends[i].usr_id === friendId[0]) {
-                roomName = friends[i].usr_nm;
-                break;
+      {/* USER'S FRIENDS LIST */}
+      <section className="Friends_List">
+        {rooms.map((room, index) => {
+          let roomName;
+          switch (room.type) {
+            case "single":
+              const friendId = room.users.filter(
+                (user) => user !== userData.userId
+              );
+              for (let i = 0; i < friends.length; i++) {
+                if (friends[i].usr_id === friendId[0]) {
+                  roomName = friends[i].usr_nm;
+                  break;
+                }
               }
-            }
-            break;
-          case "group":
-            break;
-        }
-        return <div key={index}>{roomName}</div>;
-      })}
+              break;
+            case "group":
+              break;
+          }
+          return (
+            <div key={index} className="Friends_List--Friend">
+              {/* EACH FRIEND'S CARD */}
+              <p className="Friends_List--Friend_Name">{roomName}</p>
+            </div>
+          );
+        })}
+      </section>
+      <section className="Friends_Chat"></section>
     </div>
   );
 }
