972c3e2fbca549ed64140fc993d619ed5dc03141
RohanGorla <rohangorla2010@gmail.com>
2024-12-17 02:40:46 +0530

    Add set the friends chat name according to the  room type in friends page

--- client/src/Components/Friends.jsx
+++ client/src/Components/Friends.jsx
@@ -15,7 +15,29 @@ function Friends() {
     if (!userData) return navigate("/login");
   }, []);
 
-  return <div className="Friends_Page"></div>;
+  return (
+    <div className="Friends_Page">
+      {rooms.map((room, index) => {
+        let roomName;
+        switch (room.type) {
+          case "single":
+            const friendId = room.users.filter(
+              (user) => user !== userData.userId
+            );
+            for (let i = 0; i < friends.length; i++) {
+              if (friends[i].usr_id === friendId[0]) {
+                roomName = friends[i].usr_nm;
+                break;
+              }
+            }
+            break;
+          case "group":
+            break;
+        }
+        return <div key={index}>{roomName}</div>;
+      })}
+    </div>
+  );
 }
 
 export default Friends;
