ee54e087ad05a2f2517ef21bfd0f5d2aa3c405b8
RohanGorla <rohangorla2010@gmail.com>
2024-12-18 01:14:17 +0530

    Created a friends list component and migrate all the friends list code from friends page into it to use it again in chats page

---
+++ client/src/Components/FriendsList.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
+import { IoMdPerson } from "react-icons/io";
+
+function FriendsList() {
+  /* SPECIAL VARIABLES */
+  const userData = JSON.parse(localStorage.getItem("ChatApp_UserInfo"));
+  const navigate = useNavigate();
+  const { friends, rooms } = useOutletContext();
+  /* STATE VARIABLES */
+  const [friendListSearch, setFriendListSearch] = useState("");
+
+  useEffect(() => {}, [friendListSearch]);
+
+  return (
+    <section className="Friends_List">
+      {/* SEARCH FRIEND IN FRIENDS LIST */}
+      <search className="Friends_List--Search">
+        <input
+          type="text"
+          value={friendListSearch}
+          onChange={(e) => {
+            setFriendListSearch(e.target.value);
+          }}
+          placeholder="Search friend list..."
+        ></input>
+      </search>
+      {rooms.map((room, index) => {
+        let roomName;
+        let friendsList;
+        switch (room.type) {
+          /* IF SINGLE CHAT */
+          case "single":
+            const friendId = room.users.filter(
+              (user) => user !== userData.userId
+            );
+            friendsList = friends.filter(
+              (friend) => friend.usr_id === friendId[0]
+            );
+            roomName = friendsList[0].usr_nm;
+            break;
+          /* IF GROUP CHAT */
+          case "group":
+            const friendsIdList = room.users.filter(
+              (user) => user !== userData.userId
+            );
+            friendsList = friends.filter((friend) =>
+              friendsIdList.includes(friend.usr_id)
+            );
+            roomName = room.name;
+            break;
+        }
+        return (
+          /* EACH FRIEND'S DISPLAY CARD */
+          <div
+            key={index}
+            className="Friends_List--Friend_Card"
+            onClick={() => {
+              navigate(`/user/chats/${room.roomId}`);
+            }}
+          >
+            {/* USER/GROUP DISPLAY PICTURE */}
+            <div className="Friend_Card--Image">
+              <div className="Friend_Card--Image_Icon_Container">
+                <IoMdPerson className="Friend_Card--Image_Icon" />
+              </div>
+            </div>
+            {/* CHAT INFO AND LATEST MESSAGE DISPLAY */}
+            <div className="Friend_Card--Details">
+              <div className="Friend_Card--Name_And_Time">
+                <p className="Friend_Card--Name">{roomName}</p>
+                <p className="Friend_Card--Time">07:00 PM</p>
+              </div>
+              <p className="Friend_Card--Message">This is the final message!</p>
+            </div>
+          </div>
+        );
+      })}
+    </section>
+  );
+}
+
+export default FriendsList;
