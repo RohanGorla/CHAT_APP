65148ea771ef8ed8e3cc6b227c874209f39941d2
RohanGorla <rohangorla2010@gmail.com>
2024-12-19 01:22:25 +0530

    Add a send message socket method to send messages and essential information to the server socket in chats page

--- client/src/Components/Chats.jsx
+++ client/src/Components/Chats.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import { useNavigate, useOutletContext } from "react-router-dom";
+import { useNavigate, useOutletContext, useParams } from "react-router-dom";
 import { LuSend } from "react-icons/lu";
 import FriendsList from "./FriendsList";
 import axios from "axios";
@@ -8,6 +8,7 @@ function Chats() {
   /* SPECIAL VARIABLES */
   const userData = JSON.parse(localStorage.getItem("ChatApp_UserInfo"));
   const navigate = useNavigate();
+  const { id } = useParams();
   const { socket, chat, setChat } = useOutletContext();
   const chatContainerRef = useRef(null);
   const messagesRef = useRef(null);
@@ -19,7 +20,7 @@ function Chats() {
   /* SEND MESSAGES TO THE WEB SOCKET SERVER */
   async function sendMessage(e) {
     e.preventDefault();
-    socket.emit("message_input", { username, message });
+    socket.emit("send_message", { userData, message, id, time: new Date() });
     setMessage("");
   }
 
@@ -66,19 +67,19 @@ function Chats() {
               <div
                 key={index}
                 className={
-                  username === message.name
+                  userData?.username === message.usr_nm
                     ? "Chat--Message_Card Chat--Message_Card--Own"
                     : "Chat--Message_Card Chat--Message_Card--Others"
                 }
               >
                 <p
                   className={
-                    username === message.name
+                    userData?.username === message.usr_nm
                       ? "Chat--Message_Card--Username--Inactive"
                       : "Chat--Message_Card--Username"
                   }
                 >
-                  {message.name}
+                  {message.usr_nm}
                 </p>
                 <p className="Chat--Message_Card--Message">{message.msg}</p>
               </div>
