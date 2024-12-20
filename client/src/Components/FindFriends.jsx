62073a925fb7b23a58bf447ddea398d75f0e64b8
RohanGorla <rohangorla2010@gmail.com>
2024-12-20 05:52:40 +0530

    Add a debounce function for the search input of find friends page

--- client/src/Components/FindFriends.jsx
+++ client/src/Components/FindFriends.jsx
@@ -1,19 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import { IoMdPerson } from "react-icons/io";
+import axios from "axios";
 
 function FindFriends() {
   /* SPECIAL VARIABLES */
   const navigate = useNavigate();
   const { socket } = useOutletContext();
   const userData = JSON.parse(localStorage.getItem("ChatApp_UserInfo"));
-
   /* STATE VARIABLES */
   const [user, setUser] = useState("");
 
   async function finduser() {
     socket.emit("send_request", { from: userData.userId, to: user });
   }
+
+  /* DEBOUNCE FUNCTION FOR SEARCHING USER */
+  function debounce() {
+    let timeout;
+    return function (searchInput) {
+      clearTimeout(timeout);
+      timeout = setTimeout(async () => {
+        console.log(searchInput);
+      }, 1000);
+    };
+  }
+  /* USE THE CLOSURE RETURNED BY DEBOUNCE */
+  const handleSearch = useMemo(() => debounce(), []);
 
   return (
     <div className="FindFriends_Page">
@@ -25,6 +38,7 @@ function FindFriends() {
             value={user}
             onChange={(e) => {
               setUser(e.target.value);
+              handleSearch(e.target.value);
             }}
             autoFocus
             placeholder="Search username/user ID"
