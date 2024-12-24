1174d30340e36c99e4ef7dc6eae0ea8eb69e0435
RohanGorla <rohangorla2010@gmail.com>
2024-12-24 23:10:02 +0530

    Add an empty notifications message box in notifications page

--- client/src/Components/Notifications.jsx
+++ client/src/Components/Notifications.jsx
@@ -6,7 +6,7 @@ import "../Styles/Notifications.css";
 function Notifications() {
   /* SPECIAL VARIABLES */
   const navigate = useNavigate();
-  const { socket, notifications, setNotifications } = useOutletContext();
+  const { socket, notifications } = useOutletContext();
   const userData = JSON.parse(localStorage.getItem("ChatApp_UserInfo"));
 
   /* ACCEPT FRIEND REQUEST SOCKET METHOD */
@@ -22,39 +22,47 @@ function Notifications() {
     <div className="Notifications_Page">
       <p className="Notifications--Title">NOTIFICATIONS</p>
       <div className="Notifications_Container">
-        {notifications.map((notification, index) => {
-          return (
-            <div key={index} className="Notifications_Card">
-              <div className="Notifications_Card--Image_Container">
-                <div className="Notifications_Card--Image_Icon_Container">
-                  <IoMdPerson className="Notifications_Card--Image_Icon" />
+        {notifications.length ? (
+          notifications.map((notification, index) => {
+            return (
+              <div key={index} className="Notifications_Card">
+                <div className="Notifications_Card--Image_Container">
+                  <div className="Notifications_Card--Image_Icon_Container">
+                    <IoMdPerson className="Notifications_Card--Image_Icon" />
+                  </div>
+                </div>
+                <div className="Notifications_Card--User_Details">
+                  <p className="Notifications_Card--Username">
+                    {notification.from.username}
+                  </p>
+                  <p className="Notifications_Card--Userid">
+                    {notification.from.userId}
+                  </p>
+                  <div className="Notifications_Card--Buttons">
+                    <button
+                      className="Notifications_Card--Buttons--Accept"
+                      onClick={() => acceptRequest(notification)}
+                    >
+                      Accept
+                    </button>
+                    <button
+                      className="Notifications_Card--Buttons--Reject"
+                      onClick={() => declineRequest(notification)}
+                    >
+                      Reject
+                    </button>
+                  </div>
                 </div>
               </div>
-              <div className="Notifications_Card--User_Details">
-                <p className="Notifications_Card--Username">
-                  {notification.from.username}
-                </p>
-                <p className="Notifications_Card--Userid">
-                  {notification.from.userId}
-                </p>
-                <div className="Notifications_Card--Buttons">
-                  <button
-                    className="Notifications_Card--Buttons--Accept"
-                    onClick={() => acceptRequest(notification)}
-                  >
-                    Accept
-                  </button>
-                  <button
-                    className="Notifications_Card--Buttons--Reject"
-                    onClick={() => declineRequest(notification)}
-                  >
-                    Reject
-                  </button>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          <div className="Notifications_Empty">
+            <p className="Notifications_Empty--Message">
+              When you receive any friend requests, they will be shown here!.
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
