0480ffbf4820e15165a95fd46c78d5ce76ea9936
RohanGorla <rohangorla2010@gmail.com>
2024-12-26 23:04:08 +0530

    clean up the code in notifications page

--- client/src/Components/Notifications.jsx
+++ client/src/Components/Notifications.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import { IoMdPerson } from "react-icons/io";
 import "../Styles/Notifications.css";
