2edbcfc5dded39a14e490120aad572fe35b630a3
RohanGorla <rohangorla2010@gmail.com>
2025-02-04 03:42:00 +0530

    Add a greetings list component to store the lit of all greeting messages to show in the friends home page

---
+++ client/src/assets/Greetings.js
@@ -0,0 +1,12 @@
+const greetings = [
+  "Don't leave your frens hanging! Select someone and start the kind of chat that makes you forget time... or at least kills a few minutes.",
+  "You know what they say, “A fren in need is a fren indeed.” So, be a fren and start a chat with someone.",
+  "You're looking good today! Why not spread the good vibes by starting a chat with a fren?",
+  "You're a great conversationalist, right? Prove it by starting a chat with someone.",
+  "You know what's better than a good chat? A great chat! Start one with a fren.",
+  `The art of conversation starts here. Click a name, type some words, and maybe, just maybe, your fren will reply before ${new Date().getFullYear()}.`,
+  "Click a fren's name to begin. Who knows? You might even solve the mystery of why they never reply on time.",
+  "Frens dont just follow back—they chat back too. Pick someone and start the kind of conversation that turns casual follows into meaningful connections.",
+];
+
+export default greetings;
